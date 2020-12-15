ed03642b2156f2468cebc769b4a30d4cf95ee991
dipspilani <64944055+dipspilani@users.noreply.github.com>
2020-12-15 12:19:19 +0530

    Update pre_best.py

--- pre_best.py
+++ pre_best.py
@@ -270,9 +270,7 @@ if mode=='Preprocess-1':
                      st.table(dataset.head(8))
 
                  elif choice_text == "Random Fill":
-                     
                      dataset[error[:ind-1]].fillna(random.choice(dataset[error[:ind-1]].values.tolist()) , inplace = True)
-                   
                      st.markdown(get_table_download_link(dataset), unsafe_allow_html=True)
                      st.table(dataset.head(8))
                      
