6ebe0f7e9fe3febda34b13891c05bd36799a8add
dipspilani <64944055+dipspilani@users.noreply.github.com>
2020-12-15 14:09:41 +0530

    Update pre_best.py

--- pre_best.py
+++ pre_best.py
@@ -4,7 +4,6 @@ from tpot import TPOTClassifier
 import pandas as pd
 import numpy as np
 import base64
-import cv2
 import PIL
 import random
 #xgboost ,deap , update_checker , tqdm , stopit , xgboost
