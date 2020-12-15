c92e8a942a6d29079438e17ccbf25f476ade7b2e
dipspilani <64944055+dipspilani@users.noreply.github.com>
2020-12-15 13:12:45 +0530

    Update pre_best.py

--- pre_best.py
+++ pre_best.py
@@ -273,7 +273,7 @@ if mode=='Preprocess-1':
                      
                      xxxx = dataset[error[:ind-1]]
                      xxxx.dropna(inplace = True)
-                     #dataset[error[:ind-1]].fillna(random.choice(list(xxxx)) , inplace = True)
+                     dataset[error[:ind-1]].fillna(random.choice(list(xxxx)) , inplace = True)
                      #dataset[error[:ind-1]].fillna(random.choice(dataset[dataset[error[:ind-1]] != np.nan][error[:ind-1]]) , inplace=True)
                      st.markdown(get_table_download_link(dataset), unsafe_allow_html=True)
                      st.table(dataset.head(8))
