10699e263b7c591f1bf10a39c121ed2aeb6643e3
dipspilani <64944055+dipspilani@users.noreply.github.com>
2020-12-15 12:14:37 +0530

    Update pre_best.py

--- pre_best.py
+++ pre_best.py
@@ -270,7 +270,7 @@ if mode=='Preprocess-1':
                      st.table(dataset.head(8))
 
                  elif choice_text == "Random Fill":
-                     xxxx = (dataset[error[:ind-1]])
+                     xxxx = dataset[error[:ind-1]]
                      xxxx.dropna(inplace = True)
                      dataset[error[:ind-1]].fillna(random.choice(list(xxxx)) , inplace = True)
                      #dataset[error[:ind-1]].fillna(random.choice(list(dataset[error[:ind-1]])) , inplace = True)
