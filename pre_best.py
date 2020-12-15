0bb32bba2bb5dd09a64ee17a8876ec67a1e38b37
dipspilani <64944055+dipspilani@users.noreply.github.com>
2020-12-15 21:24:08 +0530

    Update pre_best.py

--- pre_best.py
+++ pre_best.py
@@ -501,8 +501,9 @@ elif mode=='Get Best Model and its code':
                 X = dataset.loc[:,features]
                 y = dataset.loc[:,[target]]
                 scoring = ['None' , 'accuracy','average_precision', 'balanced_accuracy', 'f1', 'f1_macro', 'f1_micro', 'precision'  , 'recall' ,'roc_auc']
-                metric = st.selectbox('Choose scoring metric' , scoring)
-		st.info('Estimated time : 8-12 minutes')
+                st.info('Estimated time : 8-12 minutes')
+		metric = st.selectbox('Choose scoring metric' , scoring)
+		
                 if metric != 'None':
                     try:
                         with st.spinner('Finding the best preprocessing steps and sci-kit learn algorithm... :hourglass:'):
