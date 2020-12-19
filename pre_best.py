b1930bebcdfae2934e960efdb002e3cf57a4f129
dipspilani <64944055+dipspilani@users.noreply.github.com>
2020-12-19 20:51:18 +0530

    Update pre_best.py

--- pre_best.py
+++ pre_best.py
@@ -116,9 +116,8 @@ st.sidebar.info('6. Select "Choose Best Model and get yourself the best model ba
 fb = st.sidebar.text_area('Your feedback or suggestions or maybe if you just want to wish me a good day :)')
 submit = st.sidebar.button('Send')
 if submit and fb:
-	file = open('user_comments.csv' , 'a')
-	file.write(fb)
-	file.close()
+	ss = pd.DataFrame({'review':[fb]})
+	ss.to_csv('user_comments.csv' , mode = 'a' , header = False)
 if mode == "Code":
     st.balloons()
     st.write('https://github.com/dipspilani/automate-ml/')	
