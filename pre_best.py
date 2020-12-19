fa2fcaf1c5dc2e5fe4d95fab5e7d1f7ff32cb94c
dipspilani <64944055+dipspilani@users.noreply.github.com>
2020-12-19 13:30:03 +0530

    Update pre_best.py

--- pre_best.py
+++ pre_best.py
@@ -110,8 +110,9 @@ if mode == "Code":
 
 
 if mode=="Preprocess Text":
+	num_words = st.slider(label = 'Select the number of words(roughly)' , min_value=5 , max_value = 1000 , value = 10, step = 50)
 	st.header('Enter Text File Here')
-	text = st.text_area(label = "Text Box" , value = "" , height = 200)
+	text = st.text_area(label = "Text Box" , height = 200)
 	
 	
 	
