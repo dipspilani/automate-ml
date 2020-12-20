e0cae5395eb9dcd4cccefca5d098199dff8cdcde
dipspilani <64944055+dipspilani@users.noreply.github.com>
2020-12-21 00:46:40 +0530

    Update pre_best.py

--- pre_best.py
+++ pre_best.py
@@ -130,15 +130,15 @@ if mode=='Visualize and Cluster data':
 	st.header('Upload data here')
 	data = st.file_uploader(label="Select File (.csv or .xlsx)" , type=['csv','xlsx' , 'data'])
 	if data is not None:
-		try:
+		#try:
 			dataset = pd.read_csv(data)
 			st.success('Data Uploaded Sucessfully')
 			x = st.checkbox('Show head of dataset')
 			if x:
 				st.table(dataset.head())
 			st.pyplot(sns.pairplot(dataset))	
-		except:
-			st.error('Please choose a valid file!')
+		#except:
+		#	st.error('Please choose a valid file!')
 
 
 
