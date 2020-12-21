7408a21e8ce44c3b7e8d7413d3fc2850bedc2b04
dipspilani <64944055+dipspilani@users.noreply.github.com>
2020-12-21 12:26:49 +0530

    Update pre_best.py

--- pre_best.py
+++ pre_best.py
@@ -142,7 +142,8 @@ if mode=='Visualize and Cluster data':
 			choice = st.selectbox('Choose Operation',['None','PairPlot','2-D visualization','1-D visualization','PCA','NMF','t-SNE','Clusterize data'])
 			if choice=='PairPlot':
 				try:
-					st.pyplot(sns.pairplot(dataset))
+					with st.spinner('Creating pairplot... :hourglass:')
+						st.pyplot(sns.pairplot(dataset))
 				except:
 					st.error('Something went wrong')
 			if choice=='2-D visualization':
