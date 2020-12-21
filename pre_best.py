da62cf06290204eccc22e04b6dbdc490bc308ea4
dipspilani <64944055+dipspilani@users.noreply.github.com>
2020-12-21 16:24:09 +0530

    Update pre_best.py

--- pre_best.py
+++ pre_best.py
@@ -147,7 +147,7 @@ if mode=='Dimensionality Reduction, Visualization and Clusterization':
 					try:
 						dataset[i] = dataset[i].astype(float)
 					except:
-						dataset = dataset.drop(i)
+						dataset = dataset.drop([i])
 				from sklearn.decomposition import PCA
 				pca = PCA()
 				x = pca.fit_transform(dataset)
