85711ecbac0f57e85463191d386a0b3a4ae87a1e
dipspilani <64944055+dipspilani@users.noreply.github.com>
2020-12-21 13:43:05 +0530

    Update pre_best.py

--- pre_best.py
+++ pre_best.py
@@ -153,7 +153,7 @@ if mode=='Dimensionality Reduction, Visualization and Clusterization':
 				if cx:
 					try:
 						dataset[cx] = dataset[cx].astype(float)
-						st.plotly_chart(px.scatter(dataset,y = cx))
+						st.plotly_chart(px.scatter(dataset,x=cx))
 					except:
 						st.warning('Non-numeric column')
 			if choice=='3-D Visualization':
