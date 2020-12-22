f7cec85819937dea7868c2b66fb6a41833cd3bde
dipspilani <64944055+dipspilani@users.noreply.github.com>
2020-12-22 11:31:35 +0530

    Update pre_best.py

--- pre_best.py
+++ pre_best.py
@@ -67,7 +67,7 @@ def one_hot_encode(df,col):
     return dataset
 
 def tpot_object(metric):
-    tpot = TPOTClassifier(generations=50,
+    tpot = TPOTClassifier(generations=30,
                           population_size = 50,
                           scoring = metric,
                           disable_update_check = True,
