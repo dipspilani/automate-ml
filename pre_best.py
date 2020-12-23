8b46e329197343b48f6bd6dff5d9f0d5432ac435
dipspilani <64944055+dipspilani@users.noreply.github.com>
2020-12-23 13:13:48 +0530

    Update pre_best.py

--- pre_best.py
+++ pre_best.py
@@ -120,12 +120,12 @@ st.sidebar.info('4. Use Preprocess Images for image data (jpg,png,gif)')
 st.sidebar.info('5. Use Preprocess Text for image textual data')
 st.sidebar.info('6. Select "Choose Best Model and get yourself the best model based on desired strategy and code for the same!')
 st.sidebar.info('**Next Up:** Data Visualization')
-fb = st.sidebar.text_area('Your feedback or suggestions or maybe if you just want to wish me a good day :)')
+fb = st.sidebar.text_area('Your feedback or suggestions or maybe if you just want to wish me a good day' , key='xx')
 submit = st.sidebar.button('Send')
 if submit and fb:
 	f = open('user_comments1.txt' , 'w')
 	f.write(fb)
-	fb = ""
+	xx = ""
 
 	
 #st.header('Upload Data Here')
