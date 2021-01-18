603a28648fe28043ede37e32d228d4a797a4cbd5
dipspilani <64944055+dipspilani@users.noreply.github.com>
2021-01-18 10:56:45 +0530

    Update pre_best.py

--- pre_best.py
+++ pre_best.py
@@ -142,11 +142,11 @@ if mode=='Dimensionality Reduction, Visualization and Clusterization':
 				st.table(dataset.head())
 			choice = st.selectbox('Choose Operation',['None','PairPlot','HeatMap','3-D Visualization','2-D visualization','1-D visualization','PCA','NMF','t-SNE','Clusterize data'])
 			if choice=='HeatMap':
-				try:
+# 				try:
 					corr = dataset.corr()
 					st.pyplot(sns.heatmap(corr,annot=True))
-				except:
-					st.error('Something went wrong!')
+# 				except:
+# 					st.error('Something went wrong!')
 			if choice=='Clusterize data':
 				st.info('For visualizing clusterization, it is suggested that dimensions are reduced to 2-3. Else, you can also visualize by selection of 2-3 columns from the lot')
 				cols = list(dataset.columns)
