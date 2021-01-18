be51ba455a7f91a88af339faaba30a92633e9289
dipspilani <64944055+dipspilani@users.noreply.github.com>
2021-01-18 14:27:18 +0530

    Update pre_best.py

--- pre_best.py
+++ pre_best.py
@@ -812,10 +812,6 @@ if mode=='Preprocess-1':
                      dataset[error[:ind-1]].fillna(dataset[error[:ind-1]].median() , inplace = True)
                      st.markdown(get_table_download_link(dataset), unsafe_allow_html=True)
                      st.table(dataset.head(8))
-		elif choice=="BackFill":
-				dataset[error[:ind-1]].fillna(method='bfill' , inplace = True)
-				st.markdown(get_table_download_link(dataset), unsafe_allow_html=True)
-                     		st.table(dataset.head(8))
 
                  elif choicee=="Mode Fill":
                      dataset[error[:ind-1]].fillna(dataset[error[:ind-1]].mode()[0] , inplace = True)
